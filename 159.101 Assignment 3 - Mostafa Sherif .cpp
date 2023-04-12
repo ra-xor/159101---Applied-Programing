dcf6fe471a18405d2908477d5ee4b840ec5b2fdd
ra_XOr <mostafa.sherif.ai@gmail.com>
2023-04-13 00:20:17 +1200

    Finished Assignment 3

--- 159.101 Assignment 3 - Mostafa Sherif .cpp
+++ 159.101 Assignment 3 - Mostafa Sherif .cpp
@@ -1,13 +1,6 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <iostream>
 #include <iomanip>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -28,22 +21,48 @@ int main()
     fiveteencent = avg * 0.15;
     
     cout << fixed << setprecision(1);
-    cout<< total << endl;
     cout << "The average is " << avg << " volts." << endl;
     cout << "10% = " << tenpercent << " volts." << endl;
-    cout << "15% = " << fiveteencent << " volts." << endl; 
+    cout << "15% = " << fiveteencent << " volts." << endl << endl;
     
-    float deltaAvg[6], deltaCons[6];
-    i = 0;
-    while(i<0){
-        
+    float deltaAvg[6], deltaCons[5];
+    int p=0;
+
+    for (i=0;i<6;i++){
+        deltaAvg[i]= abs(voltages[i]-avg);
+        if (deltaAvg[i]>tenpercent){
+            p=1;
+        }
+        }
+    for (i=0;i<5;i++){
+        deltaCons[i]= abs(voltages[i]-voltages[i+1]);
+        if (deltaCons[i]>fiveteencent){
+            p=1;
+        }
+        }
+    
+    if (p==0){
+        cout << "No problems were encountered." << endl;
+    } else {
+        cout << "The following problems occurred:" << endl;
+        for (i=0;i<6;i++){
+            if (deltaAvg[i]>tenpercent){
+                cout << p <<". Voltage at hour " << i+1 << " was " << voltages[i] << " volts (difference of " << deltaAvg[i] << " volts)." << endl;
+                p++;
+            }
+        }
+        for (i=0;i<5;i++){
+            if (deltaCons[i]>fiveteencent){
+                cout << p <<". Voltage change from hour " << i+1 << " to hour " << i+2 << " was " << deltaCons[i] << " volts." << endl;
+                p++;
+            }
+        }
     }
     
     
+    //
     
-    
-    
-    
-    
+    //
+
     return 0;
 }
